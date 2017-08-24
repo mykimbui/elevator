7373f5f1958863b0a4e20202f8e6797795809232
My Kim Bui <mykimbuibui@gmail.com>
2017-08-24 11:49:03 +0800

    gsap added

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,8 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function() {
+  TweenMax.allTo('.up', {y:100, x: 100, ease: Linear.easeNone});
+  TweenMax.allTo('.down', {y:100, x: 100, ease: Linear.easeNone});
+});
