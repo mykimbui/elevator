e7876bc102682b8e340ffd504d9748d3d5113a4b
My Kim Bui <mykimbuibui@gmail.com>
2017-08-24 18:46:56 +0800

    tweening

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,6 +3,31 @@
 //= require_tree .
 
 $(document).ready(function() {
-  TweenMax.allTo('.up', {y:100, x: 100, ease: Linear.easeNone});
-  TweenMax.allTo('.down', {y:100, x: 100, ease: Linear.easeNone});
+  var upElement = $('.up-text');
+  var $containerUp = $('.containerup');
+  var $contentContainerUp = $('.content-containerup')
+  var upWidth = upElement.width();
+  var upHeight = upElement.height() + 30;
+  var containerWidth = $containerUp.width();
+  var containerHeight = $containerUp.height();
+  var upsNeeded = Math.floor(containerWidth / upWidth) * Math.ceil(containerHeight/upHeight) * 2;
+  Array(upsNeeded).fill('').map(i=>$contentContainerUp.append(upElement.clone()));
+  var linesNeeded = Math.ceil(containerHeight/upHeight);
+  TweenMax.to('.content-containerup', 8, {y: - linesNeeded * upHeight, ease: Linear.easeNone, repeat: -1});
+
+  var downElement = $('.down-text');
+  var $containerDown = $('.containerdown');
+  var $contentContainerDown = $('.content-containerdown')
+  var downWidth = downElement.width();
+  var downHeight = downElement.height() + 30;
+  var containerWidth = $containerDown.width();
+  var containerHeight = $containerDown.height();
+  var downsNeeded = Math.floor(containerWidth / downWidth) * Math.ceil(containerHeight/downHeight) * 2;
+  Array(downsNeeded).fill('').map(i=>$contentContainerDown.append(downElement.clone()));
+  var linesNeeded = Math.ceil(containerHeight/downHeight);
+  TweenMax.to('.content-containerdown', 8, {y: linesNeeded * downHeight, ease: Linear.easeNone, repeat: -1});
+
 });
+
+
+
